8abe485e2b2bb664555bc7eadaf5780e745b55c7
Han Ian <ian020805@naver.com>
2025-09-02 19:05:55 +0900

    26069 붙임성 좋은 총총이

---
+++ 26069.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    bool A_dancing = false, B_dancing = false;
+    unordered_set<string> DancingPeople;       //춤추는 사람이 들어갈 set
+    DancingPeople.insert("ChongChong");    //총총이는 이미 포함
+    string A, B;
+    cin >> N;       // N개의 기록 수 입력
+    
+    while (N-- > 0) {
+        cin >> A;
+        cin >> B;
+        if (DancingPeople.find(A) != DancingPeople.end())       // A가 춤추고 있는지
+            A_dancing = true;
+        if (DancingPeople.find(B) != DancingPeople.end())       // B가 춤추고 있는지
+            B_dancing = true;
+        
+        if (A_dancing && !B_dancing)
+            DancingPeople.insert(B);        // A만 춤추면 B도 set에 추가
+        else if(!A_dancing && B_dancing)
+            DancingPeople.insert(A);        // B만 춤추면 A도 set에 추가
+
+        A_dancing = false;
+        B_dancing = false;
+    }
+    cout << DancingPeople.size();
+    return 0;
+}
