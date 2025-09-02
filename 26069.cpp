62547e28389e54f0df7298a48896e1bd4b3c751f
Han Ian <ian020805@naver.com>
2025-09-02 19:14:22 +0900

    26069 붙임성 좋은 총총이

--- 26069.cpp
+++ 26069.cpp
@@ -9,14 +9,13 @@ int main() {
 
     int N;
     bool A_dancing = false, B_dancing = false;
+    string A, B;
     unordered_set<string> DancingPeople;       //춤추는 사람이 들어갈 set
     DancingPeople.insert("ChongChong");    //총총이는 이미 포함
-    string A, B;
     cin >> N;       // N개의 기록 수 입력
     
-    while (N-- > 0) {
-        cin >> A;
-        cin >> B;
+    while (N--) {
+        cin >> A >> B;
         if (DancingPeople.find(A) != DancingPeople.end())       // A가 춤추고 있는지
             A_dancing = true;
         if (DancingPeople.find(B) != DancingPeople.end())       // B가 춤추고 있는지
