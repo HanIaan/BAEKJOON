1fd291f550860e55bbfb86a05c3f5aa0f0cfe5e3
Han Ian <ian020805@naver.com>
2025-12-27 21:25:31 +0900

    1697 숨바꼭질

---
+++ 1697.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N, K, K_count = 100000;
+    cin >> N >> K;
+
+    if (K <= N) {
+        cout << N - K;
+        return 0;
+    }
+
+    vector<int> grid(100001, -1);
+    grid[N] = 0;
+    queue<int> q;
+
+    q.push(N);
+
+    while(!q.empty()) {
+        int n = q.front();
+        q.pop();
+        
+        if (n > 0) {
+            if (grid[n - 1] == -1) {
+                grid[n - 1] = grid[n] + 1;
+                q.push(n - 1);
+            }
+        }
+        if (n + 1 < 100001) {
+            if (grid[n + 1] == -1) {
+                grid[n + 1] = grid[n] + 1;
+                q.push(n + 1);
+            }
+        }
+        if (2 * n < 100001) {
+            if (grid[2 * n] == -1) {
+                grid[2 * n] = grid[n] + 1;
+                q.push(2 * n);
+            }
+        }
+        
+        if (n == K && K_count > grid[n]) 
+            K_count = grid[n];
+    }
+
+    cout << K_count;
+    
+    return 0;
+}
