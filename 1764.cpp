4c1758c83fa630df4fde72887bbbc2da46877792
Han Ian <ian020805@naver.com>
2025-12-28 14:29:30 +0900

    1764 듣보잡

---
+++ 1764.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+    vector<string> name(N + M), t_name;
+
+    for (int i = 0; i < N; i++) 
+        cin >> name[i];
+    for (int i = N; i < N + M; i++) 
+        cin >> name[i];
+    sort(name.begin(), name.end());
+    string target = "";
+    for (int i = 0; i < N + M - 1; i++) {
+        if (target == name[i]) continue;
+        if (name[i] == name[i + 1]) {
+            target = name[i];
+            t_name.push_back(name[i]);
+        }
+    }
+    cout << t_name.size() << endl;
+    for (auto it = t_name.begin(); it != t_name.end(); ++it)
+        cout << *it << endl;
+    return 0;
+}
