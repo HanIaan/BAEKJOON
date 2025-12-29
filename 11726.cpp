23282a43c2e762e33fa666aeecb7a89677cb52b1
Han Ian <ian020805@naver.com>
2025-12-29 11:59:53 +0900

    2xn 타일링

---
+++ 11726.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dp[1001];
+    int n;
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i < 1001; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+    cin >> n;
+    cout << dp[n];
+    return 0;
+}
